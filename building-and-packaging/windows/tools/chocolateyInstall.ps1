647f71c351ad93052bce754a254fd45e41bfef32
orieuxe <etienne.orieux.etu@univ-lille.fr>
2020-04-15 12:29:56 +0200

    windows maintainance : add packaging tools for chocolatey

---
+++ building-and-packaging/windows/tools/chocolateyInstall.ps1
@@ -0,0 +1,4 @@
+Install-ChocolateyZipPackage -PackageName 'libpointing' `
+ -Url 'https://github.com/INRIA/libpointing/releases/download/v1.0.8/libpointing-v1.0.8-Win32-VS2015.zip' `
+ -UnzipLocation 'C:/Program Files/libpointing/' `
+ -Url64 'https://github.com/INRIA/libpointing/releases/download/v1.0.8/libpointing-v1.0.8-Win64-VS2015.zip' `
